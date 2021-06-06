7e4176b4d14c19b3559d376d691c5165b08aacaf
lydianeU <lydiane.uhlen@hotmail.fr>
2021-06-06 16:11:29 +0200

    start tests

--- app/src/test/java/fr/afpa/appmeteo/ExampleUnitTest.kt
+++ app/src/test/java/fr/afpa/appmeteo/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package fr.afpa.appmeteo
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.fail
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +13,10 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun whenLyonIsGiven_ShouldDisplayLyonAsCityName() {
+        fail("not implemented yet - could use mocking")
     }
 }
